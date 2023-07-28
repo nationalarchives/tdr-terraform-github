18df0f978cf5537bceee6c7f641315ef002edace
TomJKing <tom.king@nationalarchives.gov.uk>
2023-07-28 07:44:43 +0100

    Initial skeleton setup for reference generator repo

--- root_github_repository.tf
+++ root_github_repository.tf
@@ -559,3 +559,14 @@ module "github_tdr_scripts_repository" {
     "${upper(local.environment)}_ACCOUNT_NUMBER" = local.account_id
   }
 }
+
+module "github_da_reference_generator_repository" {
+  count           = local.apply_repository
+  source          = "./da-terraform-modules/github_repository_secrets"
+  repository_name = "nationalarchives/da-reference-generator"
+  secrets = {
+    TDR_MANAGEMENT_ACCOUNT = data.aws_ssm_parameter.mgmt_account_number.value
+    TDR_SLACK_WEBHOOK      = data.aws_ssm_parameter.slack_webhook_url.value
+    TDR_WORKFLOW_PAT       = module.common_ssm_parameters.params[local.github_access_token_name].value
+  }
+}
