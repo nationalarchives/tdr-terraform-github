be0b08507ff8b90da54dd3c7caf8cd98bd959d2c
Thanh <tri.lam21@outlook.com>
2023-11-09 15:51:27 +0000

    move github role/policies to the root_github_environment.tf

--- root_github_environment.tf
+++ root_github_environment.tf
@@ -331,3 +331,146 @@ module "github_notifications_mgmt_environment" {
     ACCOUNT_NUMBER = data.aws_ssm_parameter.mgmt_account_number.value
   }
 }
+
+//New Stuff
+module "github_oidc_provider" {
+  count           = local.apply_environment
+  source          = "./da-terraform-modules/openid_provider"
+  audience        = "sts.amazonaws.com"
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  url             = "https://token.actions.githubusercontent.com"
+  tags            = local.common_tags
+}
+
+module "github_actions_deploy_lambda_policy" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_policy"
+  name   = "TDRGithubActionsDeployLambda${title(local.environment)}"
+  policy_string = templatefile("${path.module}/templates/iam_policy/deploy_lambda_github_actions.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, environment = local.environment, region = local.region
+  })
+}
+
+module "github_actions_role" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, repo_name = "tdr-*"
+  })
+  tags = local.common_tags
+  name = "TDRGithubActionsDeployLambda${title(local.environment)}"
+  policy_attachments = {
+    deploy_lambda = module.github_actions_deploy_lambda_policy[count.index].policy_arn
+  }
+}
+
+module "github_run_e2e_tests_policy" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_policy"
+  name   = "TDRGithubActionsRunE2ETestsPolicy${title(local.environment)}"
+  policy_string = templatefile("${path.module}/templates/iam_policy/github_run_e2e_tests_policy.json.tpl", {
+    environment = local.environment
+  })
+}
+
+module "github_run_e2e_tests_role" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id
+  })
+  tags = local.common_tags
+  name = "TDRGithubActionsRunE2ETestsRole${title(local.environment)}"
+  policy_attachments = {
+    run_tests_policy = module.github_run_e2e_tests_policy[count.index].policy_arn
+  }
+}
+
+module "github_update_ecs_policy" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_policy"
+  name   = "TDRGitHubECSUpdatePolicy${title(local.environment)}"
+  policy_string = templatefile("${path.module}/templates/iam_policy/github_update_ecs_policy.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, region = local.region, environment = local.environment
+  })
+}
+
+module "github_update_ecs_role" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, repo_name = "tdr-"
+  })
+  tags = local.common_tags
+  name = "TDRGitHubECSUpdateRole${title(local.environment)}"
+  policy_attachments = {
+    update_ecs_policy = module.github_update_ecs_policy[count.index].policy_arn
+  }
+}
+
+module "github_get_e2e_secrets_policy" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_policy"
+  name   = "TDRGithubActionsE2ESecretsPolicy${title(local.environment)}"
+  policy_string = templatefile("${path.module}/templates/iam_policy/github_e2e_test_secrets_policy.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, environment = local.environment
+  })
+}
+
+module "github_get_e2e_tests_secrets" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, repo_name = "tdr-"
+  })
+  tags = local.common_tags
+  name = "TDRGithubActionsGetE2ESecretsRole${title(local.environment)}"
+  policy_attachments = {
+    get_ssm_policy = module.github_get_e2e_secrets_policy[count.index].policy_arn
+  }
+}
+
+module "github_create_db_user_policy" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_policy"
+  name   = "TDRGithubActionsInvokeCreateUserLambdaPolicy${title(local.environment)}"
+  policy_string = templatefile("${path.module}/templates/iam_policy/github_invoke_create_user_lambda_policy.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, environment = local.environment
+  })
+}
+
+module "github_create_db_user_role" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id
+  })
+  tags               = local.common_tags
+  name               = "TDRGithubActionsInvokeCreateUserLambdaRole${title(local.environment)}"
+  policy_attachments = { create_user_policy = module.github_create_db_user_policy[count.index].policy_arn }
+}
+
+module "github_file_format_run_ecs_role" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
+    account_id = data.aws_caller_identity.current.account_id, repo_name = "tdr-*"
+  })
+  tags = local.common_tags
+  name = "TDRGithubActionsRunFileFormatECS${title(local.environment)}"
+  policy_attachments = {
+    run_file_format = module.github_run_file_format_build_policy[count.index].policy_arn
+  }
+}
+
+module "github_run_file_format_build_policy" {
+  count  = local.apply_environment
+  source = "./da-terraform-modules/iam_policy"
+  name   = "TDRGitHubRunFileFormatBuildPolicy${title(local.environment)}"
+  policy_string = templatefile("${path.module}/templates/iam_policy/github_run_ecs_policy.json.tpl",
+    {
+      task_definition_arn = "arn:aws:ecs:${local.region}:${data.aws_caller_identity.current.account_id}:task-definition/file-format-build-${local.environment}",
+      cluster_arn         = "arn:aws:ecs:${local.region}:${data.aws_caller_identity.current.account_id}:cluster/file_format_build_${local.environment}",
+      role_arns           = "\"arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TDRFileFormatEcsTaskRole${title(local.environment)}\", \"arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TDRFileFormatECSExecutionRole${title(local.environment)}\""
+  })
+}
