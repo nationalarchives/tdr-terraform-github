7d80f4dac18916c3bd7ae9fa8e443e6a0f14f3b5
TomJKing <tom.king@nationalarchives.gov.uk>
2026-05-15 06:50:45 +0100

    Use local profiles IAM admin roles have been retired

--- root_provider.tf
+++ root_provider.tf
@@ -1,44 +1,32 @@
-//Management account AWS provider
+// Because this is a bootstrap stack, local role config is used
 provider "aws" {
   region  = "eu-west-2"
-  profile = "management"
-}
-
-//AWS providers for TDR environment accounts
-provider "aws" {
-  alias   = "intg"
-  region  = "eu-west-2"
-  profile = "management"
-  assume_role {
-    role_arn     = "arn:aws:iam::${local.intg_account_id}:role/IAM_Admin_Role"
-    session_name = "terraform-github"
-  }
-}
-
-provider "aws" {
-  alias   = "staging"
-  region  = "eu-west-2"
-  profile = "management"
-  assume_role {
-    role_arn     = "arn:aws:iam::${local.staging_account_id}:role/IAM_Admin_Role"
-    session_name = "terraform-github"
-  }
-}
-
-provider "aws" {
-  alias   = "prod"
-  region  = "eu-west-2"
-  profile = "management"
-  assume_role {
-    role_arn     = "arn:aws:iam::${local.prod_account_id}:role/IAM_Admin_Role"
-    session_name = "terraform-github"
-  }
+  profile = "<Profile configured for account in management>"
 }
 
 provider "aws" {
   alias   = "dev"
   region  = "eu-west-2"
-  profile = "dev"
+  profile = "<Profile configured for account in dev>"
+}
+
+provider "aws" {
+  alias   = "intg"
+  region  = "eu-west-2"
+  profile = "<Profile configured for account in intg>"
+}
+
+provider "aws" {
+  alias   = "staging"
+  region  = "eu-west-2"
+  profile = "<Profile configured for account in staging>"
+
+}
+
+provider "aws" {
+  alias   = "prod"
+  region  = "eu-west-2"
+  profile = "<Profile configured for account in prod>"
 }
 
 provider "github" {
