bba1ac24ff8762556231e8a6987610f4b42426a6
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2026-09-15 20:40:25 +0100

    Add environment level secrets for e2e tests

--- root_provider.tf
+++ root_provider.tf
@@ -1,32 +1,32 @@
 // Because this is a bootstrap stack, local role config is used
 provider "aws" {
   region  = "eu-west-2"
-  profile = "<Profile configured for account in management>"
+  profile = "TNA__TDRManagement__AdministratorAccess"
 }
 
 provider "aws" {
   alias   = "dev"
   region  = "eu-west-2"
-  profile = "<Profile configured for account in dev>"
+  profile = "TNA__TDRDevelopment__AdministratorAccess"
 }
 
 provider "aws" {
   alias   = "intg"
   region  = "eu-west-2"
-  profile = "<Profile configured for account in intg>"
+  profile = "TNA__TDRIntegration__AdministratorAccess"
 }
 
 provider "aws" {
   alias   = "staging"
   region  = "eu-west-2"
-  profile = "<Profile configured for account in staging>"
+  profile = "TNA__TDRStaging__AdministratorAccess"
 
 }
 
 provider "aws" {
   alias   = "prod"
   region  = "eu-west-2"
-  profile = "<Profile configured for account in prod>"
+  profile = "TNA__TDRProduction__AdministratorAccess"
 }
 
 provider "github" {
